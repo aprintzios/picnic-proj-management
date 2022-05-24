2244100eaeb7b83a8ac41bf87c45a491b9e1192c
Alexandra Printzios <aprintzios@Alexandras-MacBook-Air.local>
2022-05-24 10:38:33 -0400

    filter/sort

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -3,23 +3,35 @@ let User = require('../models/user');
 
 async function populateDB(req, res){
     var newUser1 = new User({
-        name: "Test One",
+        name: "Person One",
         email: "testone@gmail.com",
         googleId: "12345"
     });
     var newUser2 = new User({
-        name: "Test Two",
+        name: "Person Two",
         email: "testtwo@gmail.com",
         googleId: "12346"
     });
     var newUser3 = new User({
-        name: "Test Three",
+        name: "Person Three",
         email: "testthree@gmail.com",
         googleId: "12347"
+    });
+    var newUser4 = new User({
+        name: "Person Four",
+        email: "testfour@gmail.com",
+        googleId: "12348"
+    });
+    var newUser5 = new User({
+        name: "Person Five",
+        email: "testfive@gmail.com",
+        googleId: "12349"
     });
     await newUser1.save();
     await newUser2.save();
     await newUser3.save();
+    await newUser4.save();
+    await newUser5.save();
 }
 
 module.exports = {
